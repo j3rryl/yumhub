973655d7ca7db33558f4b406391f8c9cec618d92
j3rryl <jeremymunroe0@gmail.com>
2023-12-28 19:49:48 +0300

    ....

--- app/src/main/java/com/example/yumhub/fragments/AddRecipeFragment.kt
+++ app/src/main/java/com/example/yumhub/fragments/AddRecipeFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import com.example.yumhub.R
+import com.example.yumhub.roomdb.RecipeItem
+import com.example.yumhub.roomdb.RecipeItemDatabase
+import com.example.yumhub.roomdb.RecipeItemRepository
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +28,15 @@ class AddRecipeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var inputTitle: TextInputEditText
+    private lateinit var inputDifficulty: TextInputEditText
+    private lateinit var inputType: TextInputEditText
+    private lateinit var inputIngredients: TextInputEditText
+    private lateinit var inputServings: TextInputEditText
+    private lateinit var inputPreps: TextInputEditText
+
+    private val database by lazy { RecipeItemDatabase.getDatabase(requireActivity()) }
+    val repository by lazy { RecipeItemRepository(database.recipeItemDao()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +51,70 @@ class AddRecipeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_recipe, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_recipe, container, false)
+
+        inputTitle = view.findViewById(R.id.input_title)
+
+        inputDifficulty = view.findViewById(R.id.input_difficulty)
+
+        inputType = view.findViewById(R.id.input_type)
+
+        inputIngredients = view.findViewById(R.id.input_ingredients)
+
+        inputPreps = view.findViewById(R.id.input_prep)
+
+        inputServings = view.findViewById(R.id.input_servings)
+
+
+        val saveButton: Button = view.findViewById(R.id.save_recipe_button)
+
+        saveButton.setOnClickListener{
+            var titleText: String = inputTitle.text.toString().trim()
+            var difficultyText: String = inputDifficulty.text.toString().trim()
+            var typeText: String = inputType.text.toString().trim()
+            var ingredientsText: String = inputIngredients.text.toString().trim()
+            var prepText: String = inputPreps.text.toString().trim()
+            var servingsText: String = inputServings.text.toString().trim()
+            var recipeQuantity: Int = if (servingsText.isNotEmpty()) {
+                try {
+                    servingsText.toInt()
+                } catch (e: NumberFormatException) {
+                    // Handle the case where the input is not a valid integer
+                    // For example, show an error message or log the issue
+                    0 // Default value if the conversion fails
+                }
+            } else {
+                1 // Default value if servingsText is empty
+            }
+
+            val item = RecipeItem (
+                recipe_title = titleText,
+                recipe_type = typeText,
+                recipe_difficulty = difficultyText,
+                recipe_ingredients = ingredientsText,
+                recipe_preparation_steps= prepText,
+                recipe_quantity= recipeQuantity,
+                recipe_image = R.drawable.chicken
+            )
+            lifecycleScope.launch {
+                repository.insertRecipeItem(item)
+            }
+            val newFragment = RecipeFragment()
+
+            // Begin the fragment transaction
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+
+            // Replace the current fragment with the new one
+            transaction.replace(R.id.frame_layout, newFragment)
+
+            // Add the transaction to the back stack (optional, enables back navigation)
+            transaction.addToBackStack(null)
+
+            // Commit the transaction
+            transaction.commit()
+        }
+
+        return view
     }
 
     companion object {
